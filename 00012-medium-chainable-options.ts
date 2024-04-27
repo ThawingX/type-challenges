22859e54530f74f6cf8ba690031249512ac417c6
Dylan Long <thawingx@gmail.com>
2024-04-27 20:37:13 +0800

    feat(fourth day):  The first stage of the practive is completed。 Temporaily pengding

--- 00012-medium-chainable-options.ts
+++ 00012-medium-chainable-options.ts
@@ -45,7 +45,10 @@ type Expected3 = {
 
 
 // ============= Your Code Here =============
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
-}
+type Chainable<T extends Record<string, any> = {}> = {
+  option: <K extends string, V>(
+  key: K extends keyof T ? never : K,
+  value: V,
+  ) => Chainable<Omit<T, K> & Record<K, V>>
+  get(): T
+  }
